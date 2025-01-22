f0dde0ae4ed1f8cd5819ad13ca835bbf63ede868
LavdrimIslami <lavdrim.islami280@gmail.com>
2025-01-21 23:18:21 -0500

    made it function based

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
 import cv2 as cv
+import numpy as np
 
-print(cv.__version__)
+def basicOps():
+    ### BASIC OPERATIONS ###
+    img = cv.imread("tetrominoe.png")
+
+    assert img is not None, "cant be read"
+
+    #accessing pixel values
+
+    px = img[100,100]
+
+    print(px)
+
+
+    print("modify a pixel value")
+    img[100,100] = [255,255,255]
+    print(img[100,100])
+
+    print("\nImage Properties access")
+    print("image shape", img.shape)
+    print("image size", img.size)
+    print("image datatype", img.dtype)
+
+    
+
+
+
+
+def main():
+    basicOps()
+
+if __name__ == "__main__":
+    main()
